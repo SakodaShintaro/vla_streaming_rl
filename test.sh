4ca1760501e2e698170d1f3b525f8fc2f3adcb58
Shintaro Sakoda <sakoda@keio.jp>
2026-06-18 18:53:04 +0900

    Fixed test.sh
    Signed-off-by: Shintaro Sakoda <sakoda@keio.jp>

--- test.sh
+++ test.sh
@@ -4,3 +4,5 @@ set -eux
 uv run python scripts/train.py exp_name=test debug=true batch_size=2 agent_type=off_policy network_class=actor_critic_with_action_value
 uv run python scripts/train.py exp_name=test debug=true batch_size=2 agent_type=streaming network_class=actor_critic_with_action_value
 uv run python scripts/train.py exp_name=test debug=true batch_size=2 agent_type=streaming network_class=vlm_actor_critic_with_action_value
+uv run python scripts/train.py agent=simlingo env=carla_special_case exp_name=test debug=true
+uv run python scripts/train.py agent=libero_pi05 env=libero exp_name=test debug=true
