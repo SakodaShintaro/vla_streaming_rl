1ea9a1b1ce105e034aa6cbcc7be459493d279bf6
Shintaro Sakoda <rgbygscrsedppbwg@gmail.com>
2026-06-25 13:33:57 +0900

    Fixed test.sh
    Signed-off-by: Shintaro Sakoda <rgbygscrsedppbwg@gmail.com>

--- test.sh
+++ test.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 set -eux
 
-uv run python scripts/train.py exp_name=test debug=true batch_size=2 agent_type=off_policy network_class=actor_critic_with_action_value
-uv run python scripts/train.py exp_name=test debug=true batch_size=2 agent_type=streaming network_class=actor_critic_with_action_value
-uv run python scripts/train.py exp_name=test debug=true batch_size=2 agent_type=streaming network_class=vlm_actor_critic_with_action_value
+uv run python scripts/train.py exp_name=test debug=true batch_size=2 learning_mode=off_policy network_class=actor_critic_with_action_value
+uv run python scripts/train.py exp_name=test debug=true batch_size=2 learning_mode=streaming network_class=actor_critic_with_action_value
+uv run python scripts/train.py exp_name=test debug=true batch_size=2 learning_mode=streaming network_class=vlm_actor_critic_with_action_value
 uv run python scripts/train.py agent=simlingo env=carla_special_case exp_name=test debug=true
 uv run python scripts/train.py agent=libero_pi05 env=libero exp_name=test debug=true
