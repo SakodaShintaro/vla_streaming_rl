5f4524394d31d07ee7cf5ca3f2b1d8576c32d9db
Shintaro Sakoda <rgbygscrsedppbwg@gmail.com>
2026-07-11 13:12:21 +0900

    Removed ColorPanel-v0
    Signed-off-by: Shintaro Sakoda <rgbygscrsedppbwg@gmail.com>

--- src/vla_streaming_rl/wrappers.py
+++ src/vla_streaming_rl/wrappers.py
@@ -10,7 +10,6 @@ import hydra
 import minigrid
 import numpy as np
 
-from vla_streaming_rl.envs.color_panel_env import ColorPanelEnv
 from vla_streaming_rl.envs.letter_tracing_env import LetterTracingEnv
 from vla_streaming_rl.envs.stl10_panel_env import STL10PanelEnv
 from vla_streaming_rl.envs.tracking_square_env import TrackingSquareEnv
@@ -213,14 +212,6 @@ def make_env(env_id: str, env_factory, result_dir) -> gym.Env:
         env = gym.wrappers.RecordEpisodeStatistics(env)
         env = TransposeAndNormalizeObs(env)
         env.unwrapped.eval_range = 20
-        return env
-
-    elif env_id == "ColorPanel-v0":
-        env = ColorPanelEnv(render_mode="rgb_array")
-        env = gym.wrappers.RecordEpisodeStatistics(env)
-        env = TransposeAndNormalizeObs(env)
-        env.unwrapped.eval_range = 20
-        env.unwrapped.parse_action_text = _color_panel_parse_action
         return env
 
     elif env_id == "STL10Panel-v0":
