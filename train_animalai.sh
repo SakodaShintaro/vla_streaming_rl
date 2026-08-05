b7b2d9e75addc4033a93942287738cf3586ed099
Shintaro Sakoda <rgbygscrsedppbwg@gmail.com>
2026-08-05 19:02:00 +0900

    Fixed to use vlm
    Signed-off-by: Shintaro Sakoda <rgbygscrsedppbwg@gmail.com>

--- train_animalai.sh
+++ train_animalai.sh
@@ -22,7 +22,7 @@ export DISPLAY
 
 uv run python scripts/train.py \
   agent=standard \
-  network_class=actor_critic_with_action_value \
+  network_class=vlm_actor_critic_with_action_value \
   learning_mode=off_policy \
   batch_size=16 \
   actor_lr=1e-5 \
